2b99c1dc772b9ca2ac1ee1f30a4d631899ed1160
gustavogbc@gmail.com <gustavogbc@gmail.com@94a42c09-fd7f-aa33-5994-252b535afb41>
2014-06-15 18:35:49 +0000

    enviando pacotes de 160 bytes

--- COS765Server/src/com/cos765/server/Server.java
+++ COS765Server/src/com/cos765/server/Server.java
@@ -1,57 +1,79 @@
 package com.cos765.server;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import org.apache.commons.io.FileUtils;
+
 public class Server {
 
 	public static int TRANSMISSION_TIME = 20; // 20ms entre pacotes
 	private static int PORT = 15000;
 	private static int PACKET_SIZE = 160; // 160 bytes por pacote
-
+			
 	public static void main(String[] args) throws Exception {
 		DatagramSocket serverSocket = new DatagramSocket(PORT);
-		// byte[] receiveData = new byte[PACKET_SIZE];
-		byte[] sendData = new byte[PACKET_SIZE];
+		int bytesRead = 0;
+		byte[] receiveData = new byte[PACKET_SIZE]; // o nome do arquivo desejado pelo cliente
+		byte[] sendData = new byte[PACKET_SIZE]; // o conteúdo do arquivo a ser
+													// enviado para o cliente
+		String fileName;	
+				
+		while (true) {
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);						
+			serverSocket.receive(receivePacket); // recebe nome da fonte de dados (arquivo)		
+			fileName = new String(receivePacket.getData());
 
-		BufferedReader br = null;
-		try {
-			String line;
-			br = new BufferedReader(new FileReader("C:\\test.txt"));
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
+			InetAddress iPAddress = receivePacket.getAddress();				
+			int port = receivePacket.getPort();
+			
+			try
+			{
+				ByteArrayInputStream bis = new ByteArrayInputStream(
+						FileUtils.readFileToByteArray(new File("C:\\test.txt")));
+				
+				while (bytesRead != -1) {
+					sendData = new byte[PACKET_SIZE];
+					bytesRead = bis.read(sendData, 0, PACKET_SIZE);
+					// Enviar os dados
+					DatagramPacket sendPacket = new DatagramPacket(sendData,
+							sendData.length, iPAddress, port);
+					serverSocket.send(sendPacket);
+				}					
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null)
-					br.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-
-		// while (true) {
-		// DatagramPacket receivePacket = new DatagramPacket(receiveData,
-		// receiveData.length);
-		// serverSocket.receive(receivePacket);
-		// String sentence = new String(receivePacket.getData());
-		// InetAddress iPAddress = receivePacket.getAddress();
-		//
-		// int port = receivePacket.getPort();
-		// String capitalizedSentence = sentence.toUpperCase();
-		// sendData = capitalizedSentence.getBytes();
-		//
-		// DatagramPacket sendPacket = new DatagramPacket(sendData,
-		// sendData.length, iPAddress, port);
-		// serverSocket.send(sendPacket);
-		// }
-		serverSocket.close();
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}		
+		}		
+		
+		
+//			 while (true) {
+//			 // Ao iniciar, o programa de transferˆencia de ´audio deve receber
+//			 // informa¸c˜oes sobre a fonte de dados
+//			 // que gerar´a as amostras a serem transferidas.
+//			 DatagramPacket receivePacket = new DatagramPacket(receiveData,
+//			 receiveData.length);
+//			 serverSocket.receive(receivePacket); // recebe nome da fonte de
+//			 // dados (arquivo)
+//			
+//			 String sentence = new String(receivePacket.getData());
+//			 InetAddress iPAddress = receivePacket.getAddress();
+//			
+//			 int port = receivePacket.getPort();
+//			 // String capitalizedSentence = sentence.toUpperCase();
+//			 // sendData = capitalizedSentence.getBytes();			
+//			 DatagramPacket sendPacket = new DatagramPacket(sendData,
+//			 sendData.length, iPAddress, port);
+//			 serverSocket.send(sendPacket);
+//			 }
+//		 serverSocket.close(); // fechando o socket aberto para envio do
+		// arquivo
 	}
-
 }
