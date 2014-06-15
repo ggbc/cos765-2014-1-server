a34f7586432eecacb0a84a60731d81809db4a270
gustavogbc@gmail.com <gustavogbc@gmail.com@94a42c09-fd7f-aa33-5994-252b535afb41>
2014-06-15 14:36:08 +0000

    lendo arquivo SOMENTE sem restricao de tamanho

--- COS765Server/src/com/cos765/server/Server.java
+++ COS765Server/src/com/cos765/server/Server.java
@@ -1,5 +1,8 @@
 package com.cos765.server;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -12,24 +15,42 @@ public class Server {
 
 	public static void main(String[] args) throws Exception {
 		DatagramSocket serverSocket = new DatagramSocket(PORT);
-//		byte[] receiveData = new byte[PACKET_SIZE];
+		// byte[] receiveData = new byte[PACKET_SIZE];
 		byte[] sendData = new byte[PACKET_SIZE];
 
-//		while (true) {
-//			DatagramPacket receivePacket = new DatagramPacket(receiveData,
-//					receiveData.length);
-//			serverSocket.receive(receivePacket);
-//			String sentence = new String(receivePacket.getData());
-//			InetAddress iPAddress = receivePacket.getAddress();
-//
-//			int port = receivePacket.getPort();
-//			String capitalizedSentence = sentence.toUpperCase();
-//			sendData = capitalizedSentence.getBytes();
-//
-//			DatagramPacket sendPacket = new DatagramPacket(sendData,
-//					sendData.length, iPAddress, port);
-//			serverSocket.send(sendPacket);
-//		}
+		BufferedReader br = null;
+		try {
+			String line;
+			br = new BufferedReader(new FileReader("C:\\test.txt"));
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		// while (true) {
+		// DatagramPacket receivePacket = new DatagramPacket(receiveData,
+		// receiveData.length);
+		// serverSocket.receive(receivePacket);
+		// String sentence = new String(receivePacket.getData());
+		// InetAddress iPAddress = receivePacket.getAddress();
+		//
+		// int port = receivePacket.getPort();
+		// String capitalizedSentence = sentence.toUpperCase();
+		// sendData = capitalizedSentence.getBytes();
+		//
+		// DatagramPacket sendPacket = new DatagramPacket(sendData,
+		// sendData.length, iPAddress, port);
+		// serverSocket.send(sendPacket);
+		// }
 		serverSocket.close();
 	}
 
