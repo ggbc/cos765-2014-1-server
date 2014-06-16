c6725b27059124b0d43cd95ff04a947e0089b0bf
gustavogbc@gmail.com <gustavogbc@gmail.com@94a42c09-fd7f-aa33-5994-252b535afb41>
2014-06-16 17:24:17 +0000

    tirando comentários

--- COS765Server/src/com/cos765/server/Server.java
+++ COS765Server/src/com/cos765/server/Server.java
@@ -1,90 +1,67 @@
 package com.cos765.server;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Date;
-
-import javax.swing.plaf.SliderUI;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 
 public class Server {
 
 	public static int TRANSMISSION_TIME = 20; // 20ms entre pacotes
 	private static int PORT = 15000;
 	private static int PACKET_SIZE = 160; // 160 bytes por pacote
-			
+
 	public static void main(String[] args) throws Exception {
 		DatagramSocket serverSocket = new DatagramSocket(PORT);
 		int bytesRead = 0;
-		byte[] receiveData = new byte[PACKET_SIZE]; // o nome do arquivo desejado pelo cliente
+		byte[] receiveData = new byte[PACKET_SIZE]; // o nome do arquivo
+													// desejado pelo cliente
 		byte[] sendData = new byte[PACKET_SIZE]; // o conteúdo do arquivo a ser
 													// enviado para o cliente
-		String fileName;	
-				
-		while (true) {
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);						
-			serverSocket.receive(receivePacket); // recebe nome da fonte de dados (arquivo)		
-			fileName = new String(receivePacket.getData());
-			fileName = "C://" + fileName; // VER ISSO DEPOIS. Não estou conseguindo mandar o nome do arquivo com duas barras /!
+		String fileName;
 
-			InetAddress iPAddress = receivePacket.getAddress();				
+		while (true) {
+			DatagramPacket receivePacket = new DatagramPacket(receiveData,
+					receiveData.length);
+			serverSocket.receive(receivePacket); // recebe nome da fonte de
+													// dados (arquivo)
+			fileName = new String(receivePacket.getData());
+			fileName = "D://Setor//" + fileName; // VER ISSO DEPOIS. Não estou
+													// conseguindo mandar o nome
+													// do arquivo com duas
+													// barras /!
+
+			InetAddress iPAddress = receivePacket.getAddress();
 			int port = receivePacket.getPort();
-			
-			try
-			{
+
+			try {
 				ByteArrayInputStream bis = new ByteArrayInputStream(
-						FileUtils.readFileToByteArray(new File("C:\\test.txt")));
-				
+						FileUtils.readFileToByteArray(new File(
+								"D:\\setor\\abc.txt")));
+
 				while (bytesRead != -1) {
 					sendData = new byte[PACKET_SIZE];
-					sendData[0]++; // campo PackNumber para reordenação no host destino 
+					sendData[0]++; // campo PackNumber para reordenação no host
+									// destino. São permitidos até 127 pacotes
+									// (tamanho máximo de arquivo: 19MB)
 					bytesRead = bis.read(sendData, 0, PACKET_SIZE);
-					
-//					System.out.println((new Date()).getTime());
+
+					// System.out.println((new Date()).getTime());
 					Thread.sleep(20);
-//					System.out.println((new Date()).getTime());					
-					
+					// System.out.println((new Date()).getTime());
+
 					// Enviar os dados
 					DatagramPacket sendPacket = new DatagramPacket(sendData,
 							sendData.length, iPAddress, port);
 					serverSocket.send(sendPacket);
-				}					
-			}
-			catch (IOException e)
-			{
+				}
+			} catch (IOException e) {
 				e.printStackTrace();
-			}		
-		}		
-		
-		
-//			 while (true) {
-//			 // Ao iniciar, o programa de transferˆencia de ´audio deve receber
-//			 // informa¸c˜oes sobre a fonte de dados
-//			 // que gerar´a as amostras a serem transferidas.
-//			 DatagramPacket receivePacket = new DatagramPacket(receiveData,
-//			 receiveData.length);
-//			 serverSocket.receive(receivePacket); // recebe nome da fonte de
-//			 // dados (arquivo)
-//			
-//			 String sentence = new String(receivePacket.getData());
-//			 InetAddress iPAddress = receivePacket.getAddress();
-//			
-//			 int port = receivePacket.getPort();
-//			 // String capitalizedSentence = sentence.toUpperCase();
-//			 // sendData = capitalizedSentence.getBytes();			
-//			 DatagramPacket sendPacket = new DatagramPacket(sendData,
-//			 sendData.length, iPAddress, port);
-//			 serverSocket.send(sendPacket);
-//			 }
-//		 serverSocket.close(); // fechando o socket aberto para envio do
-		// arquivo
+			}
+		}
 	}
 }
